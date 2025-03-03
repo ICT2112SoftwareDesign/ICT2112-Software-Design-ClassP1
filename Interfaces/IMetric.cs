8d0d1f56acb3ffc4561205ad86dfe911720d7c32
Zheng Feng <cheahzhengfeng@gmail.com>
2025-03-03 21:22:11 +0800

    setting up Interfaces and Control Directories

---
+++ Interfaces/IMetric.cs
@@ -0,0 +1,14 @@
+public interface IMetric
+{
+        /// <summary>
+        /// Gets the name of the metric.
+        /// </summary>
+        /// <returns>String representing the metric name.</returns>
+        string GetMetricName();
+
+        /// <summary>
+        /// Gets the computed value of the metric.
+        /// </summary>
+        /// <returns>Object representing the metric's computed value.</returns>
+        object GetValue();
+}
