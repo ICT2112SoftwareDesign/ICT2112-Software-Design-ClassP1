8235f498318f7e64b6998c651bda2b01c246ff81
kellyjw030 <kellyjw2002@gmail.com>
2025-03-13 00:28:41 +0800

    added interfaces
    iReturnFormDatabase has temporary methods that will be removed once iManufacturer and iItem interfaces are done.

---
+++ CleanBrilliantCompany/Interfaces/iReturnFormDatabase.cs
@@ -0,0 +1,23 @@
+﻿using CleanBrilliantCompany.Models;
+
+namespace CleanBrilliantCompany.Interfaces
+{
+	public interface iReturnFormDatabase<T>
+	{
+		T? getDatabaseQueryStatus(Task<T?> task);
+		List<T> getDatabaseQueryStatus(Task<List<T>> task);
+		
+		bool getDatabaseQueryStatus(Task<bool> task);
+
+        // TEMP!!-------------------
+        string getDatabaseQueryStatus(Task<string> task);
+		int getDatabaseQueryStatus(Task<int> task);
+		// TEMP!!-------------------
+
+		Task<List<T>> findAll();
+		Task<T?> findByItemId(int id);
+		Task<bool> delete(int id);
+		Task<T?> insert(T entity);
+
+	}
+}
---
+++ CleanBrilliantCompany/Interfaces/iReturnFormQuery.cs
@@ -0,0 +1,17 @@
+﻿using CleanBrilliantCompany.Models;
+
+namespace CleanBrilliantCompany.Interfaces
+{
+	public interface iReturnFormQuery
+	{
+        public List<ReturnForm> displayReturnForms();
+
+        public ReturnForm? getReturnFormById(int itemId);
+
+        public bool deleteReturnForm(int returnId);
+
+		public ReturnForm? insertReturnForm(ReturnForm model);
+
+
+	}
+}
