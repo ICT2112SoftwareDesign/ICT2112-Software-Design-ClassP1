c22b58fc3cee4c4baac18daf95820c571a454456
AbsoluteGibs <zhpskelvinchew@gmail.com>
2025-03-14 23:49:53 +0800

    add more to gitignore

--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceef5cc26872a430f6bcc109b13163a2fdb1ebdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b461cc5e93e6fb6392ebe8ca8fef4a9c9b0c087")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
