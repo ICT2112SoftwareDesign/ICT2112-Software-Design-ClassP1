4f096473c816d48634f7d8489c551185b6748b24
reginetan <reginetan297@gmail.com>
2025-03-15 19:39:13 +0800

    cleaned up navigation

--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b294e26f5f317a8c843c278901383ca36be390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc0c2439e9462352bda6f45984faa5e57501444")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
