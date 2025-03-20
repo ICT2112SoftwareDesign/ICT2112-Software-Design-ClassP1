3c091fd9d5b8d602afc4d29014bfb3c9093a505d
xwlow <2301949@sit.singaporetech.edu.sg>
2025-03-20 11:34:50 +0800

    Revert ":)"
    This reverts commit 41345dd1baf8597852a2c917b06d3c01e5ad1bd1.

--- CleanBrilliantCompany/Interfaces/IObserver.cs
+++
@@ -1,6 +0,0 @@
-using CleanBrilliantCompany.Models.Entity;
-
-public interface IObserver
-{
-    void Update(Item item);
-}
--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b99f02a5a2af7136ecd7c2e797532a3984df5a7")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda10f722c49940d1d045b1d14e29abd49d0f94f")]
->>>>>>> module2-team3-expiryTransactionTest
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
