82ba4e5596909fd2312d48b767fdaaf5deb27855
Rayner <rayner_tan@rocketmail.com>
2025-03-21 05:25:22 +0800

    Stop tracking appsettings.json

--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b35bdae0991b771abc7b8df6145cf4ae226c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497556b599fd3a2ece4399162ab7ba119a86f3d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
