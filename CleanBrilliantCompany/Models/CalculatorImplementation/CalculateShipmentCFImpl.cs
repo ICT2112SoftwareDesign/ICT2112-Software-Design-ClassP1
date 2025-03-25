288dfa5eb8ffd9c0f44e0a5b0e76251da2cc7ba9
AbsoluteGibs <zhpskelvinchew@gmail.com>
2025-03-26 00:58:48 +0800

    updated to use getItemCFbyItemId with class diagram

--- CleanBrilliantCompany/Models/CalculatorImplementation/CalculateShipmentCFImpl.cs
+++ CleanBrilliantCompany/Models/CalculatorImplementation/CalculateShipmentCFImpl.cs
@@ -6,26 +6,27 @@ namespace CleanBrilliantCompany.Models.CalculatorImplementation
     public class CalculateShipmentCFImpl : IShipmentCFCalculator
     {
         private readonly IOrderCFManagement _orderCFManagement; // for inserting order record
-        private readonly IItemCFManagement _itemCFManagement; // for retrieving item CF
+        private readonly IItemCF _itemCF; // for retrieving item CF
 
         //private readonly IOrder _order; // for retrieving list of item Ids
 
         // Calculate the carbon footprint for a shipment.
-        public CalculateShipmentCFImpl(IOrderCFManagement orderCFManagement, IItemCFManagement itemCFManagement)
+        public CalculateShipmentCFImpl(IOrderCFManagement orderCFManagement, IItemCF itemCF)
         {
             _orderCFManagement = orderCFManagement;
-            _itemCFManagement = itemCFManagement;
+            _itemCF = itemCF;
         }
 
         public float CalculateCarbonFootprint(ShipmentSDM shipment)
         {
             float totalEmission = 0.0f;
 
-            // List<int> orderItemsIdList = _orderCFManagement.getItemIdList() // waiting for implementation from mod 1 side
-            // foreach(int itemId in orderItemsIdList){
-            //     float itemCF = _itemCFManagement.getItemCFbyItemId(itemId);
-            //     totalEmission += itemCF;
-            // }
+            List<int> orderItemsIdList = new List<int>();
+            //List<int> orderItemsIdList = _orderCFManagement.getItemIdList() // waiting for implementation from mod 1 side
+            foreach(int itemId in orderItemsIdList){
+                double itemCF = _itemCF.getItemCarbonFootprintByItemId(itemId);
+                totalEmission += (float)itemCF;
+            }
 
             // Iterate over each route segment of the shipment.
             foreach (var segment in shipment.RouteSegments)
