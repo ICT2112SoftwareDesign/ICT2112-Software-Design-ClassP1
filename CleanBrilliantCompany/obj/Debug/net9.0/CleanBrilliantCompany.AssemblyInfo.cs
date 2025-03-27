3171395f88dacdd161ab37d97ec65443dd907f01
reginetan <reginetan297@gmail.com>
2025-03-27 18:25:23 +0800

    #6 Moved into respective folders for merging

--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3381700fb65fa548aa3cfc7a60553ac5d2d5eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edbb2d39c33ab3881d4ce5aade6582fefa4e1d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
