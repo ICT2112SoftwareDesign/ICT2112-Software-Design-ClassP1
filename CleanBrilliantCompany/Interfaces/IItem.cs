cefab63cb37b7ddc1316f922dad78d756635786a
Arian <43028630+hamtamSP@users.noreply.github.com>
2025-03-28 19:21:00 +0800

    Sync Stage

--- CleanBrilliantCompany/Interfaces/IItem.cs
+++
@@ -1,9 +0,0 @@
-using CleanBrilliantCompany.Models.Entity;
-
-namespace CleanBrilliantCompany.Interfaces
-{
-    public interface IItem
-    {
-        Task<Item> getItemById(int itemId); 
-    }
-}
--- CleanBrilliantCompany/Interfaces/IProductDatabase.cs
+++
@@ -1,9 +0,0 @@
-using CleanBrilliantCompany.Models.Entity;
-using Microsoft.Data.SqlClient;
-namespace CleanBrilliantCompany.Interfaces
-{
-    public interface iProductDatabase
-    {
-        bool getDatabaseQueryStatus(SqlDataReader reader, int rowsAffected = -1);
-    }
-}
