d435317148c2fb304f0db6a61cee7fc24792a09a
jarrettyeocy <jarrett.yeo@gmail.com>
2025-04-02 23:54:38 +0800

    shipping + orderfulfilment update

--- CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
+++ CleanBrilliantCompany/obj/Debug/net9.0/CleanBrilliantCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95bc7b357c188e7254afb035ea09d4666684d87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103f12ce6c830d2c0e2432c762c18dd6c2a92a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanBrilliantCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
