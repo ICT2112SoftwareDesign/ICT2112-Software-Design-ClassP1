97fcf48df7e7ca574809590418789613b7c84092
kellyjw030 <kellyjw2002@gmail.com>
2025-04-05 00:22:14 +0800

    Update iReturnFormQuery.cs

--- CleanBrilliantCompany/Interfaces/iReturnFormQuery.cs
+++ CleanBrilliantCompany/Interfaces/iReturnFormQuery.cs
@@ -5,8 +5,12 @@ namespace CleanBrilliantCompany.Interfaces
 	public interface iReturnFormQuery
 	{
         public List<ReturnForm> displayReturnForms();
-
+        public List<Item> displayAllToReturnItems();
         public ReturnForm? getReturnFormById(int itemId);
+
+        public bool deleteReturnForm(int productId, int itemId);
+        public ReturnForm generateReturnForm(int productId, int itemId);
+        public Task<ReturnForm?> sendReturnForm(ReturnForm model);
 
     }
 }
