0331c6b31ce1ed04359d407874fc5ba862664f42
XINJINGG <xinjing-02@outlook.com>
2025-04-05 12:39:14 +0800

    comment another line

--- CleanBrilliantCompany/Interfaces/IOrderFufilment.cs
+++ CleanBrilliantCompany/Interfaces/IOrderFufilment.cs
@@ -4,7 +4,6 @@ namespace CleanBrilliantCompany.Interfaces
 {
     public interface IOrderFufilment
     {
-        // still have to reflect on product quantity
         List<Item> adjustInventory(int orderId, Dictionary<int, int> orderProducts); 
         void processCancelledOrder(int orderId);
     }
