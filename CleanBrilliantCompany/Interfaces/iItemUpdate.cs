5a7c879282636acf98f0bd85eb626e19f7639d93
XINJINGG <xinjing-02@outlook.com>
2025-04-05 12:38:00 +0800

    remove comment

--- CleanBrilliantCompany/Interfaces/iItemUpdate.cs
+++ CleanBrilliantCompany/Interfaces/iItemUpdate.cs
@@ -5,7 +5,6 @@ namespace CleanBrilliantCompany.Interfaces
     public interface IItemUpdate
     {
         Task<bool> updateItemStatus(int itemId, int? reservationId, int? orderId, int? transferId, int? returnId, ItemStatus status); 
-        // to add update quantity method from product
         void updateProductQuantity(int productId, int quantity, string arithmeticOperations);
     }
 }
